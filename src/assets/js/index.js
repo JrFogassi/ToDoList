7d0503647f04f93a4bdf1175240f9485565d6d71
Junior Fogassi <junior.fogassi@valtech.com>
2022-05-05 13:13:29 -0300

    comportamento principal da ToDo List

---
+++ src/assets/js/index.js
@@ -0,0 +1,52 @@
+//armazena as tasks, funciona como um banco
+let dataTask = [
+    {newTask: "Malhar", taskStatus: ""},  
+    {newTask: "Estudar", taskStatus:"checked"}
+    ];
+
+    // adiciona uma task ao html, a tela
+const addTask = (newTask, taskStatus) => {
+    const task = document.createElement('label');
+    task.classList.add ('todo-tasks');
+    task.innerHTML = `<input type=checkbox ${taskStatus}> <div>${newTask}</div> <input type=button>`;
+
+    document.getElementById("tasks-list").appendChild(task);
+}
+
+//remove a ultima task criada quando a tela é atualizada
+const clearTaskField = () => {
+    const tasksList = document.getElementById("tasks-list");
+    while (tasksList.firstChild){
+        tasksList.removeChild(tasksList.lastChild);
+    }
+}
+
+//atualiza o campo de tasks
+const refreshTasksField = () => {
+    clearTaskField();
+    dataTask.forEach (task => addTask (task.newTask, task.taskStatus));
+}
+
+//cria a task ao clicar enter
+const createTask = (event) => {
+    const create = event.key;
+    const taskBox = event.target.value;
+    if (create === "Enter"){
+        dataTask.push({newTask: taskBox, taskStatus:"checked"});
+        refreshTasksField();
+        
+        //apaga o campo de texto
+        event.target.value = "";
+    } 
+}
+
+document.getElementById("add-task").addEventListener("keypress", createTask);
+
+refreshTasksField();
+
+
+
+//createElement para criar um campo no HTML, como um label
+//classList.add vai criar uma classe para o elemento selecionado
+//innerHTML vai incluir elementos no HTML
+//getElementById.appendChild vai selecionar um Id no HTML e o apChild vai criar um filho para tal Id
